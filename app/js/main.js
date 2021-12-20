6a4f7fc1fc2194be9624648d6b7c42286dc926e0
MartinEde-n <evgen.vezstuden@gmail.com>
2021-12-20 16:41:43 +0300

    Added Home_Page

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.clients__inner').slick({
+		slidesToShow:5,
+		slidesToScroll:5,
+		arrows:false,
+		variableWidth: true,
+		autoplay:true,
+		autoplaySpeed:0,
+    speed: 5000,
+    pauseOnDotsHover: true
+  });
+});
